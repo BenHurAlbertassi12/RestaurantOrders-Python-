485171efff90d8b3c58a8675a7205387d5fcdba6
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-06-16 16:24:36 -0300

    def test_ingredient()

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,32 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    # definindo os ingredientes
+    req_1_ingrediente_1 = Ingredient("manteiga")
+    req_1_ingrediente_2 = Ingredient("manteiga")
+    req_1_ingrediente_3 = Ingredient("bacon")
+
+    #  verificações simples de igualdade
+    assert hash(req_1_ingrediente_1) == hash(req_1_ingrediente_2)
+    assert hash(req_1_ingrediente_1) != hash(req_1_ingrediente_3)
+    assert req_1_ingrediente_1 == req_1_ingrediente_2
+    assert req_1_ingrediente_1 != req_1_ingrediente_3
+    assert req_1_ingrediente_2.name == "manteiga"
+
+    # verifica a representação em dormato de string
+    assert repr(req_1_ingrediente_3) == "Ingredient('bacon')"
+
+    # verifica se o conjunto dos ingredientes
+    assert Ingredient("frango").restrictions == {
+        Restriction.ANIMAL_MEAT, 
+        Restriction.ANIMAL_DERIVED
+    }
+
+    # verifica se o conjunto dos ingredientes
+    assert Ingredient("creme de leite").restrictions == {
+        Restriction.LACTOSE, 
+        Restriction.ANIMAL_DERIVED
+    }
+
