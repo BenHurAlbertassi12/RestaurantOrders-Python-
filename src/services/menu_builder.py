19ed375de52fa167b8d74a3ae1ce5022131382fb
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-06-16 22:09:41 -0300

    get_main_menu, não esta passando em tudo

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -24,6 +24,31 @@ class MenuBuilder:
 
         self.inventory.consume_recipe(curr_dish.recipe)
 
-    # Req 4
     def get_main_menu(self, restriction=None) -> pd.DataFrame:
-        pass
+
+        # Lista de descrições dos itens do menu
+        desc_itens = ["dish_name", "ingredients", "price", "restrictions"]
+
+        # Criação de uma lista de dicionários com as informações dos pratos
+        pratos_menu = [
+
+            # Mapeamento das dscrições com os valores correspondentes
+            # dict converte as tuplas resultantes em dicionarios, permitindo
+            # organizar as informações dos pratos do menu de forma estruturada.
+            dict(
+                # zip combina as descrições dos itens com os valores iguais
+                zip(desc_itens, [
+                    index.name,
+                    index.price,
+                    index.get_restrictions(),
+                    index.get_ingredients()
+                ])
+            )
+            # Iteração sobre os pratos do menu
+            for index in self.menu_data.dishes
+            # Verifica se o prato não possui a restrição especificada
+            if restriction not in index.get_restrictions()
+        ]
+
+        # Retorna um DataFrame do pandas contendo as informações dos pratos
+        return pd.DataFrame(pratos_menu)
